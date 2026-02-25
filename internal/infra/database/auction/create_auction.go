8af7115a0df64bc3163591b7dc40076b6251e951
Xavier PMS <rafaelpmaciel@gmail.com>
2026-02-25 16:56:10 -0300

    Closing action automatically

--- internal/infra/database/auction/create_auction.go
+++ internal/infra/database/auction/create_auction.go
@@ -5,7 +5,10 @@ import (
 	"fullcycle-auction_go/configuration/logger"
 	"fullcycle-auction_go/internal/entity/auction_entity"
 	"fullcycle-auction_go/internal/internal_error"
+	"os"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -19,13 +22,22 @@ type AuctionEntityMongo struct {
 	Timestamp   int64                           `bson:"timestamp"`
 }
 type AuctionRepository struct {
-	Collection *mongo.Collection
+	Collection                  *mongo.Collection
+	auctionInterval             time.Duration
+	insertAuctionFn             func(ctx context.Context, auction *AuctionEntityMongo) error
+	closeAuctionIfExpiredByIDFn func(ctx context.Context, auctionId string, auctionEndTime time.Time) error
 }
 
 func NewAuctionRepository(database *mongo.Database) *AuctionRepository {
-	return &AuctionRepository{
-		Collection: database.Collection("auctions"),
+	repository := &AuctionRepository{
+		Collection:      database.Collection("auctions"),
+		auctionInterval: getAuctionInterval(),
 	}
+
+	repository.insertAuctionFn = repository.insertAuction
+	repository.closeAuctionIfExpiredByIDFn = repository.closeAuctionIfExpiredByID
+
+	return repository
 }
 
 func (ar *AuctionRepository) CreateAuction(
@@ -40,11 +52,61 @@ func (ar *AuctionRepository) CreateAuction(
 		Status:      auctionEntity.Status,
 		Timestamp:   auctionEntity.Timestamp.Unix(),
 	}
-	_, err := ar.Collection.InsertOne(ctx, auctionEntityMongo)
+
+	err := ar.insertAuctionFn(ctx, auctionEntityMongo)
 	if err != nil {
 		logger.Error("Error trying to insert auction", err)
 		return internal_error.NewInternalServerError("Error trying to insert auction")
 	}
 
+	go ar.autoCloseAuction(auctionEntity.Id, auctionEntity.Timestamp)
+
 	return nil
+}
+
+func (ar *AuctionRepository) insertAuction(ctx context.Context, auction *AuctionEntityMongo) error {
+	_, err := ar.Collection.InsertOne(ctx, auction)
+	return err
+}
+
+func (ar *AuctionRepository) autoCloseAuction(auctionId string, auctionTimestamp time.Time) {
+	auctionEndTime := calculateAuctionEndTime(auctionTimestamp, ar.auctionInterval)
+	waitTime := time.Until(auctionEndTime)
+	if waitTime > 0 {
+		time.Sleep(waitTime)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	if err := ar.closeAuctionIfExpiredByIDFn(ctx, auctionId, auctionEndTime); err != nil {
+		logger.Error("Error trying to close auction automatically", err)
+	}
+}
+
+func (ar *AuctionRepository) closeAuctionIfExpiredByID(ctx context.Context, auctionId string, auctionEndTime time.Time) error {
+	filter := bson.M{
+		"_id":       auctionId,
+		"status":    auction_entity.Active,
+		"timestamp": bson.M{"$lte": auctionEndTime.Unix()},
+	}
+
+	update := bson.M{"$set": bson.M{"status": auction_entity.Completed}}
+
+	_, err := ar.Collection.UpdateOne(ctx, filter, update)
+	return err
+}
+
+func getAuctionInterval() time.Duration {
+	auctionInterval := os.Getenv("AUCTION_INTERVAL")
+	duration, err := time.ParseDuration(auctionInterval)
+	if err != nil {
+		return time.Minute * 5
+	}
+
+	return duration
+}
+
+func calculateAuctionEndTime(auctionTimestamp time.Time, auctionInterval time.Duration) time.Time {
+	return auctionTimestamp.Add(auctionInterval)
 }
